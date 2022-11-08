37d3dd97f60c900f0c8d643c792ee6e89f5ecb20
mona93810 <monabommakanti@gmail.com>
2022-11-08 16:39:53 +0530

    nw

--- src/main/java/com/example/demo/Usercontroller.java
+++ src/main/java/com/example/demo/Usercontroller.java
@@ -1,5 +1,35 @@
-public class Usercontroller {
-    public String demo () {
-        return "Hello World";
-    }
+@RestController
+public class Controller {
+
+	// One syntax to implement a
+	// GET method
+	@GetMapping("/")
+	public String home()
+	{
+		String str
+			= "<html><body><font color=\"green\">"
+			+ "<h1>WELCOME To GeeksForGeeks</h1>"
+			+ "</font></body></html>";
+		return str;
+	}
+
+	// Another syntax to implement a
+	// GET method
+	@RequestMapping(
+		method = { RequestMethod.GET },
+		value = { "/gfg" })
+
+	public String info()
+	{
+		String str2
+			= "<html><body><font color=\"green\">"
+			+ "<h2>GeeksForGeeks is a Computer"
+			+ " Science portal for Geeks. "
+			+ "This portal has been "
+			+ "created to provide well written, "
+			+ "well thought and well explained "
+			+ "solutions for selected questions."
+			+ "</h2></font></body></html>";
+		return str2;
+	}
 }
